95955193324aa90b30d0109ab91ebdb323f9b746
moussafia <moussafia.mohammed1999@gmail.com>
2026-06-05 19:51:11 +0100

    init project microservice backend- spring cloud

---
+++ READMe.md
@@ -0,0 +1,2 @@
+<h1>Smart walth AI</h1>
+<img src="images/smartwealthAI_architecture.png">
---
+++ smart-walth-ai-backend/test-service/src/main/java/ma/enset/testservice/TestController.java
@@ -0,0 +1,18 @@
+package ma.enset.testservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> getAllProducts() {
+        return ResponseEntity.ok(List.of("test1", "test2", "test3"));
+    }
+}
